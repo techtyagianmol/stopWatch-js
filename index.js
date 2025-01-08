0b0e936331f870510338fd151730fc1b97c8ff02
Anmol Tyagi <tyagimonty259@gmail.com>
2025-01-08 23:55:35 +0530

    updates

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ function stopWatch(){
             hours++;
         }
     }
+
+    let h = hours <10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+   let s = seconds < 10 ? "0" + seconds : seconds ;
+   
+   
+   displayTime.innerHTML = h +":"+ m +":"+ s;
 }
 function watchStart(){
     if(timer!==null){
@@ -19,4 +26,14 @@ function watchStart(){
     }
    timer = setInterval(stopWatch,1000);
 
+}
+function watchStop(){
+    clearInterval(timer);
+    
+}
+
+function watchReset(){
+    clearInterval(timer);
+    [seconds,minutes,hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
 }
